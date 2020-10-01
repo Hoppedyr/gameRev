dae96fad14206ee39cd43c7a79fc412ce9843a18
Rasmus Jarnborg Friis <rasmusjf@hotmail.com>
2020-10-01 10:54:01 +0200

    index styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="container-fluid">
-    <Logo />
+  <div class="container-fluid bg-dark">
     <h1 class="title">gameRev</h1>
     <div class="row">
-      <div v-for="game in games" :key="game.id" class="col-4">
-        <div class="card">
+      <div v-for="game in games" :key="game.id" class="col-3 pb-2">
+        <div class="card bg-secondary">
           <nuxt-link :to="{ path: '/game', query: { id: game.id } }">
-            <div class="card-body">
+            <img
+              :src="game.background_image"
+              alt="Girl in a jacket"
+              class="card-img-top"
+            />
+            <div class="card-text text-white">
               <p>{{ game.name }}</p>
-              <img
-                :src="game.background_image"
-                alt="Girl in a jacket"
-                class="img-fluid mx-auto"
-              />
             </div>
           </nuxt-link>
         </div>
